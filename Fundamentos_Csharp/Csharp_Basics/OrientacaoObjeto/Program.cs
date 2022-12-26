69711674310a3ef3d3b28bdf500b982c10bcdc4d
JustRonis <ronaldoluizdev@gmail.com>
2022-12-26 10:07:01 -0300

    subindo heranca e classe selada

--- Fundamentos_Csharp/Csharp_Basics/OrientacaoObjeto/Program.cs
+++ Fundamentos_Csharp/Csharp_Basics/OrientacaoObjeto/Program.cs
@@ -15,9 +15,12 @@ Polimorfismo fornece a capacidade de herdar métodos da classe pai, e sobreescre
 
 //namespace Cadastro;
 
+using System.Reflection;
+using System.Security.AccessControl;
 using Cadastro;
+using Heranca;
 
-// Aqui temos um exemplo de encapsulamento, afinal as propriedades ID e Descricao são privadas, é somente a própria classe tem um metodo que pode alterar estes valores. Sendo assim, estamos usando modificadores de acesso para gaarantir a integridade das propriedades ID e Descricao 
+// Aqui temos um exemplo de encapsulamento, afinal as propriedades ID e Descricao são privadas, e somente a própria classe tem um metodo que pode alterar estes valores. Sendo assim, estamos usando modificadores de acesso para gaarantir a integridade das propriedades ID e Descricao 
 namespace Cadastro
 {
 	public class Produto
@@ -53,6 +56,33 @@ namespace Cadastro
 	}
 }
 
+
+namespace Heranca
+{
+	public class Pessoa
+	{
+		public string PrimeiroNome { get; set; }
+		public string SegundoNome { get; set; }
+		public string CPF { get; set; }
+	}
+
+	public class PessoaDeficiente : Pessoa
+	{
+		public string DescricaoDoenca { get; set; }
+	}
+}
+
+
+namespace ClasseSelada
+{
+	public sealed class Configuracao //utilizando a palavra reservada sealed, estamos criando uma classe que não permite ser herdada. Ou seja, eu não posso criar uma outra classe herdando da classe Configuracao
+	{
+		public string cmd = "ipconfig";
+	}
+	
+	// public class TesteSealed : Configuracao  - Isto não é permitido, afinal, Configuracao é uma classe selada
+}
+
 namespace OrientacaoObjeto
 {
 	public class Program
@@ -71,6 +101,25 @@ namespace OrientacaoObjeto
 			var estoque_produto2 = produto2.Estoque;
 			
 			Console.WriteLine("Id do produto: " + produto2.GetId() + " Descrição: " + produto2.GetDescricao()+ " Estoque: " + estoque_produto2);
+			
+			//------------------------------------------------------------------------------------------------------------------------------------------------------
+
+			var pessoa1 = new Pessoa();
+			pessoa1.PrimeiroNome = "Ronaldo";
+			pessoa1.SegundoNome = "Luiz";
+			pessoa1.CPF = "01010101";
+			
+			Console.WriteLine($"Pessoa. Nome: {pessoa1.PrimeiroNome} {pessoa1.SegundoNome}. CPF = {pessoa1.CPF}");
+
+			var pessoa2 = new PessoaDeficiente();
+
+			pessoa2.PrimeiroNome = "Rodrigo";
+			pessoa2.SegundoNome = "Santos";
+			pessoa2.CPF = "02020202";
+			pessoa2.DescricaoDoenca = "Cadeirante";
+			
+			Console.WriteLine($"Pessoa. Nome: {pessoa2.PrimeiroNome} {pessoa2.SegundoNome}. CPF = {pessoa2.CPF}. Descrição deficiencia = {pessoa2.DescricaoDoenca}");
+			
 
 
 		}
