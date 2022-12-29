c9d7f36552bfa5e0955a24e82aa34274cf4ab77c
JustRonis <ronaldoluizdev@gmail.com>
2022-12-29 12:16:34 -0300

    Conversao de valor

--- Fundamentos_Csharp/Csharp_Basics/OrientacaoObjeto/Program.cs
+++ Fundamentos_Csharp/Csharp_Basics/OrientacaoObjeto/Program.cs
@@ -179,6 +179,7 @@ namespace Inteface // Uma interface é uma especie de contrato, onde definimos a
 }
 
 
+
 namespace OrientacaoObjeto
 {
 	public class Program
@@ -253,6 +254,33 @@ namespace OrientacaoObjeto
 			notificacaoFuncionario.Notificar("Caro colaborador, lhe desejamos um feliz natal e um prospero ano novo");
 			
 			
+			
+			//Conversão de valor------------------------------------------------------------------------------------------------------------------------------------------------------
+
+			int numero = 1;
+			int numero1 = int.Parse("1");
+			int numero2 = Convert.ToInt32("2");
+			int numero3 = Convert.ToInt32(null); // Foi feita uma conversão de null para int, que retorna 0. Então por boa prática é melhor usar o int.parse para converter o valor. Pois o mesmo é mais performatico. Se converter um valor para null retornar 0 for aceitavel para a aplicação, então tudo bem usar o Convert, caso contrario é melhor usar o int.Parse mesmo.
+			Console.WriteLine(numero3);
+			bool verdadeiro = bool.Parse("true");
+
+			var numero4 = "a"; // Pode mudar o valor de numero4 para simular a condição do if abaixo
+
+			if (int.TryParse(numero4, out int numero4Convertido)) // O metodo TryParse tenta converter o numero e devolve um booleano. True em caso de sucesso, ou false caso não conseguir converter o número
+			{
+				Console.WriteLine("Numero convertido com sucesso");
+			}
+			else
+			{
+				Console.WriteLine("Não foi possivel converter o número");
+			}
+
+
+
+
+			
+
+
 
 		}
 	}
