f871a3baae250e7b9f942cf2ddaf0e1001d2ed6c
JustRonis <ronaldoluizdev@gmail.com>
2023-01-02 20:37:40 -0300

    iniciando com asp net core

---
+++ Iniciando_AspNetCore/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Iniciando_AspNetCore.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
