c81f5b890d23263cded84e7ff2ca4e74c671b8a4
= <=>
2023-02-06 11:01:22 -0300

    Subindo estrutura do Curso csharp

---
+++ CSharp_BasicoAoAvancado/CursoCSharp/CursoCSharp/Fundamentos/PrimeiroPrograma.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.Fundamentos
+{
+    public class PrimeiroPrograma
+    {
+        public static void Executar()
+        {
+            System.Console.WriteLine("Criação de pastas implica no nome do namespace");
+        }
+    }
+}
---
+++ CSharp_BasicoAoAvancado/CursoCSharp/CursoCSharp/Program.cs
@@ -0,0 +1,18 @@
+﻿using CursoCSharp.Fundamentos;
+using System;
+
+namespace CursoCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var central = new CentralDeExercicios(new Dictionary<string, Action>()
+            {
+                {"Primeiro Programa - Fundamentos", PrimeiroPrograma.Executar},
+            });
+
+            central.SelecionarEExecutar();
+        }
+    }
+}
